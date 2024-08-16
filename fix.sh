65971f3bdd62decf88233bed5224743f7db3b10f
Retrometer <157724406+Retrometer@users.noreply.github.com>
2024-08-16 23:16:08 +0200

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,9 @@
+# if one of both of the following errors appear:
+# /home/<NAME>/.local/share/applications/sandpile.desktop doesn't exist
+# qtpaths not found
+# then run this file.
+# After running it, launch sp_installl.sh again.
+
+mkdir ~/.local/share/applications
+sudo pacman -Syy qt5-tools
+rm -rf ~/sandpile
